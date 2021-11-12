cf690a94c2879ac76f4f5dde11fe484e7bae5870
marcelinoNet <marcelinoneto34@gmail.com>
2021-11-12 10:48:17 -0300

    first commit

---
+++ backend/src/main/java/com/devsuperior/Desafio_DevSuperior/DesafioDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Desafio_DevSuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDevSuperiorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/Desafio_DevSuperior/DesafioDevSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Desafio_DevSuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDevSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
